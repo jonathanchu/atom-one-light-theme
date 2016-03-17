107a78e31af2aaa4e60b300b2355ab38a7f36cc8
Jonathan Chu <jonathan.chu@me.com>
2016-03-17 00:03:53 -0400

    :art:

--- atom-one-light-theme.el
+++ atom-one-light-theme.el
@@ -15,6 +15,37 @@
 (deftheme atom-one-light
   "Atom One Light - An Emacs port of the Atom One Light theme from Atom.io.")
 
+(defvar atom-one-light-colors-alist
+  '(("atom-one-light-accent"   . "#526FFF")
+    ("atom-one-light-fg"       . "#383A42")
+    ("atom-one-light-bg"       . "#FAFAFA")
+    ("atom-one-light-bg-1"     . "#121417")  ;; TODO
+    ("atom-one-light-bg-hl"    . "#2F343D")  ;; TODO
+    ("atom-one-light-mono-1"   . "#383A42")
+    ("atom-one-light-mono-2"   . "#696C77")
+    ("atom-one-light-mono-3"   . "#A0A1A7")
+    ("atom-one-light-cyan"     . "#0184BC")
+    ("atom-one-light-blue"     . "#4078F2")
+    ("atom-one-light-purple"   . "#A626A4")
+    ("atom-one-light-green"    . "#50A14F")
+    ("atom-one-light-red-1"    . "#E45649")
+    ("atom-one-light-red-2"    . "#CA1243")
+    ("atom-one-light-orange-1" . "#986801")
+    ("atom-one-light-orange-2" . "#C18401")
+    ("atom-one-light-gray"     . "#3E4451")  ;; TODO
+    ("atom-one-light-silver"   . "#AAAAAA")  ;; TODO
+    ("atom-one-light-black"    . "#0F1011")) ;; TODO
+  "List of Atom One Light colors.")
+
+(defmacro atom-one-light-with-color-variables (&rest body)
+  "Bind the colors list around BODY."
+  (declare (indent 0))
+  `(let ((class '((class color) (min-colors 89)))
+         ,@ (mapcar (lambda (cons)
+                      (list (intern (car cons)) (cdr cons)))
+                    atom-one-light-colors-alist))
+     ,@body))
+
 ;; theme code goes here
 
 ;;;###autoload
