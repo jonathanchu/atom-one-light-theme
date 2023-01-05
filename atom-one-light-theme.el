3492223b61bd10a2a23400a73d44bf3a740c5094
Alynx Zhou <alynx.zhou@gmail.com>
2023-01-05 18:41:14 +0800

    Update colors to match Atom One Light

--- atom-one-light-theme.el
+++ atom-one-light-theme.el
@@ -16,25 +16,29 @@
   "Atom One Light - An Emacs port of the Atom One Light theme from Atom.io.")
 
 (defvar atom-one-light-colors-alist
-  '(("atom-one-light-accent"   . "#526FFF")
-    ("atom-one-light-fg"       . "#383A42")
-    ("atom-one-light-bg"       . "#FAFAFA")
-    ("atom-one-light-bg-1"     . "#E5E5E6")  ;; MEBEE?
-    ("atom-one-light-bg-hl"    . "#CECECE")  ;; MEBEE?
-    ("atom-one-light-mono-1"   . "#383A42")
-    ("atom-one-light-mono-2"   . "#696C77")
-    ("atom-one-light-mono-3"   . "#A0A1A7")
-    ("atom-one-light-cyan"     . "#0184BC")
-    ("atom-one-light-blue"     . "#4078F2")
-    ("atom-one-light-purple"   . "#A626A4")
-    ("atom-one-light-green"    . "#50A14F")
-    ("atom-one-light-red-1"    . "#E45649")
-    ("atom-one-light-red-2"    . "#CA1243")
-    ("atom-one-light-orange-1" . "#986801")
-    ("atom-one-light-orange-2" . "#C18401")
-    ("atom-one-light-gray"     . "#EDEDED")  ;; MEBEE?
-    ("atom-one-light-silver"   . "#AAAAAA")  ;; TODO
-    ("atom-one-light-black"    . "#0F1011")) ;; TODO
+  '(("atom-one-light-accent"        . "#526EFF")
+    ("atom-one-light-fg"            . "#383A42")
+    ("atom-one-light-bg"            . "#FAFAFA")
+    ("atom-one-light-bg-1"          . "#E5E5E6")  ;; MEBEE?
+    ("atom-one-light-bg-hl"         . "#F0F0F1")
+    ("atom-one-light-gutter"        . "#C2C2C3")
+    ("atom-one-light-mono-1"        . "#383A42")
+    ("atom-one-light-mono-2"        . "#696C77")
+    ("atom-one-light-mono-3"        . "#A0A1A7")
+    ("atom-one-light-cyan"          . "#0184BC")
+    ("atom-one-light-blue"          . "#4078F2")
+    ("atom-one-light-purple"        . "#A626A4")
+    ("atom-one-light-green"         . "#50A14F")
+    ("atom-one-light-red-1"         . "#E45649")
+    ("atom-one-light-red-2"         . "#CA1243")
+    ("atom-one-light-orange-1"      . "#B76B01")
+    ("atom-one-light-orange-2"      . "#CB7701")
+    ("atom-one-light-gray"          . "#E5E5E6")
+    ("atom-one-light-silver"        . "#EAEAEB")
+    ("atom-one-light-black"         . "#424243")
+    ("atom-one-light-ui-fg"         . "#424243")
+    ("atom-one-light-level-3-color" . "#EAEAEB")
+    ("atom-one-light-border"        . "#DBDBDC"))
   "List of Atom One Light colors.")
 
 (defmacro atom-one-light-with-color-variables (&rest body)
